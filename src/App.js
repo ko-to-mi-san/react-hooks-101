6b9c2600b9f979f3197ed7d30585d2f713ceb675
Kotomi <piitun3232@gmail.com>
2022-02-13 23:09:02 +0900

    useState使う

--- src/App.js
+++ src/App.js
@@ -6,11 +6,31 @@ const App = () => {
   const increment = () => setCount(count + 1)
   const decrement = () => setCount(count - 1)
 
+  const increment2 = () => setCount(previousCount => previousCount + 1)
+  const decrement2 = () => setCount(previousCount => previousCount - 1)
+
+  const resetButton = () => setCount(0)
+  const double = () => setCount(count * 2)
+  const devide3 = () => setCount(previousCount =>
+    previousCount % 3 === 0 ? previousCount / 3 : previousCount
+  )
+
   return (
     <>
       <div>count: {count}</div>
-      <button onClick={increment}>+1</button>
-      <button onClick={decrement}>-1</button>
+      <div>
+        <button onClick={increment}>+1</button>
+        <button onClick={decrement}>-1</button>
+      </div>
+      <div>
+        <button onClick={increment2}>+1</button>
+        <button onClick={decrement2}>-1</button>
+      </div>
+      <div>
+        <button onClick={resetButton}>Reset</button>
+        <button onClick={double}>×2</button>
+        <button onClick={devide3}>3の倍数の時だけ3で割る</button>
+      </div>
     </>
   )
 }
