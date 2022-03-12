f79bfd937279d169508c4800e8bd3f3f9c7ebb95
Kotomi <piitun3232@gmail.com>
2022-03-12 21:25:59 +0900

    リファクタリング

---
+++ src/components/Events.jsx
@@ -0,0 +1,14 @@
+<h4 className='mt-5'>イベント一覧</h4>
+        <table className='table table-hover'>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>タイトル</th>
+              <th>ボディ</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {state.map((event, index) => (<Event key={index} event={event} dispatch={dispatch} />))}
+          </tbody>
+        </table>
